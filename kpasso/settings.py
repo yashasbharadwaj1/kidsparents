67710677837920866752bb87b63aec82b94cd893
yashas a bharadwaj <yashasbharadwaj9@gmail.com>
2022-06-07 15:31:23 +0530

    configured sqlite3

--- kpasso/settings.py
+++ kpasso/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'kpasso.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase',
+        'NAME': 'db.sqlite3',
     }
 }
 
