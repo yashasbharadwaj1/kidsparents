e7fb4b6ed4405bf0b1988c80bb75f9efea9412e1
yashas a bharadwaj <yashasbharadwaj9@gmail.com>
2022-06-07 14:18:44 +0530

    database change

--- kpasso/settings.py
+++ kpasso/settings.py
@@ -80,19 +80,13 @@ WSGI_APPLICATION = 'kpasso.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+
 DATABASES = {
-'default': {
-'ENGINE': 'django.db.backends.mysql',
-'NAME': 'fsproject',
-'USER': 'root',
-'PASSWORD': "",
-'HOST': "",
-'PORT': "",
-'OPTIONS': {
-'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-}
-}
-}
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+       }
+    }
 
 
 # Password validation
