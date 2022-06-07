db3773beefe362722bfc0f1cb1ee8d2560168e49
yashas a bharadwaj <yashasbharadwaj9@gmail.com>
2022-06-07 15:17:46 +0530

    deploy

--- kpasso/settings.py
+++ kpasso/settings.py
@@ -84,10 +84,9 @@ WSGI_APPLICATION = 'kpasso.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'mydatabase',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
