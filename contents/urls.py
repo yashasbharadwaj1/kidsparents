fd2a5f1803428047334c7234dc42023ec5baedd3
yashasbharadwaj1 <yashasbharadwaj6@gmail.com>
2022-08-25 13:47:46 +0530

    fixing new serach bug

--- contents/urls.py
+++ contents/urls.py
@@ -6,8 +6,8 @@ app_name = 'contents'
 urlpatterns = [
     path('', views.home, name='homepage'),
     path('search/',views.post_search,name='post_search'),
-    path('searchbloodbanks/',views.bloodbank_search,name='blood_search'),
-    path('searchblooddonors/',views.blooddonor_search,name='blood_search'),
+    path('searchbloodbanks/',views.bloodbank_search,name='bloodbank_search'),
+    path('searchblooddonors/',views.blooddonor_search,name='blooddonar_search'),
     path('<slug:post>/', views.post_single, name='post_single'),
     path('category/<category>/', views.CatListView.as_view(), name='category'),
 ]
